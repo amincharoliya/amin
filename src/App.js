09c2a3c271e092b80fd7ddf8804053db705f2e22
Amin Charoliya <amincharoliya@gmail.com>
2021-09-22 00:15:17 +0530

    Added dark mode switch

--- src/App.js
+++ src/App.js
@@ -1,16 +1,51 @@
 import React, { Component } from 'react';
+import { Route, BrowserRouter as Router, Switch } from 'react-router-dom';
+
 import Home from './home';
 import Notfound from './notfound';
-import { Route, BrowserRouter as Router, Switch } from 'react-router-dom';
+import { setTheme } from './utils/setTheme';
 
 import './App.css';
 
-class App extends Component {
-  render() {
+const App = () => {
+    const [togClass, setToggleClass] = React.useState('dark');
+    let theme = localStorage.getItem('theme');
+
+    const SwitchTheme = () => {
+        if (localStorage.getItem('theme') === 'theme-dark') {
+            setTheme('theme-light');
+            setToggleClass('light')
+        } else {
+            setTheme('theme-dark');
+            setToggleClass('dark')
+        }
+    }
+
+    React.useEffect( () => {
+        if (localStorage.getItem('theme') === 'theme-dark') {
+            setToggleClass('dark')
+            setTheme('theme-dark');
+        } else if (localStorage.getItem('theme') === 'theme-light') {
+            setToggleClass('light')
+            setTheme('theme-light');
+        }
+    }, [theme])
+
     return (
       <Router>
                 <main>
                     <div className="main-area">
+                      <nav className="theme-toggle">
+                        {
+                            togClass === "light" ?
+                            <input type="checkbox" id="toggle" className="toggle--checkbox" onClick={SwitchTheme} checked />
+                            :
+                            <input type="checkbox" id="toggle" className="toggle--checkbox" onClick={SwitchTheme} />
+                        }
+                        <label htmlFor="toggle" className="toggle--label">
+                            <span className="toggle--label-background"></span>
+                        </label>
+                      </nav>
                       <Switch>
                           <Route exact path="/" component={Home} />
                           <Route component={Notfound} />
@@ -20,7 +55,6 @@ class App extends Component {
             </Router>
       
     );
-  }
 }
 
 export default App;
